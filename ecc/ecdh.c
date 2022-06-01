80b2dc34fa2e11ceb15292fb2d21109fb7e12c16
Vlad <root@sony>
2022-06-01 21:45:03 +0300

    ...

--- ecc/ecdh.c
+++ ecc/ecdh.c
@@ -250,8 +250,8 @@ error_t ecdhImportPrivateKey(EcdhContext *context, const uint8_t *key,
       if(!error)
       {
          //Derive the public key from the private key
-         error = ecGeneratePublicKey(&context->params, &context->da.d,
-            &context->qa.q);
+         error = ecGeneratePublicKey(&context->params, &context->da,
+            &context->qa);
       }
    }
 #if (X25519_SUPPORT == ENABLED)
